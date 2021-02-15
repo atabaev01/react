62acc5af3884ea288a619319a368229218a0d3dd
Atabaev <blackpanda7@mail.ru>
2021-02-15 17:20:33 +0600

    onclickdone

--- src/component/Todoitem.js
+++ src/component/Todoitem.js
@@ -19,34 +19,19 @@ class Todoitem extends Component {
         this.setState(this.updateState, this.postUpdate)
     }
 
+    onClickDone = () =>{
+        this.setState((state)=>{
+            return {done: !state.done}
+        })
+    }
+
     render() {
         let className = this.state.important ? "list-group-item todo-item active" : "todo-item list-group-item"
-        const spanClassName = " done"
-        state = {
-            important: false
-        };
-        updateState = (state) => {
-            return{
-                important: !state.important
-            }
-        }
-    
-        postUpdate = ()=>{
-            console.log(this.state.important);
-        }
-    
-    
-        onClick = () => {
-            this.setState(this.updateState, this.postUpdate)
-        }
-        render (){
-            const className
-        }
-    
+        const spanClassName = this.state.done ? "text done":"text"
         return (
             <li className={className}>
                 <span onClick={this.onClick}  className={spanClassName}>{this.props.children}</span>
-                <span className="btn btn-success">Done</span>
+                <button onClick={this.onClickDone} className="btn btn-success">Done</button>
             </li>
             
         );
